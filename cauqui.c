3bc983c50a01713ecb4934d9f27dc7ba91518dcc
joaoamorimgamer <cachorro11_nando@proton.me>
2026-02-08 18:38:13 -0300

    Alguns updates no arquivo cauqui.c

--- cauqui.c
+++ cauqui.c
@@ -8,7 +8,7 @@ int main()
     int segundoNum;
     int metodo;
 
-    printf("Bem vindo a calculadora! Essa é a melhor calculadora feita, simplesmente porque ela é muito\n leve e desinchada. Ela não tem suporte para operações que você literalmente nunca vai usar na sua vida além das aulas de matemática.\n");
+    printf("Bem vindo a calculadora! Essa é a melhor calculadora feita, simplesmente porque ela é muito\n leve e desinchada. Ela não tem suporte para operações que você literalmente nunca vai usar na sua vida.\n");
     printf("\nprimeiro número? ");
     scanf("%d", &primeiroNum);
     printf("\nsegundo número? ");
@@ -18,16 +18,16 @@ int main()
     scanf("%d", &metodo);
     if(metodo == 1)
     {
-        printf("o resultado é %d. Pronto, salvei você uma questão da sua tarefa!\n", primeiroNum + segundoNum);
+        printf("o resultado é %d.\n", primeiroNum + segundoNum);
     }
     else if(metodo == 2){
-        printf("o resultado é %d. Pronto, salvei você uma questão da sua tarefa!\n", primeiroNum - segundoNum);
+        printf("o resultado é %d.\n", primeiroNum - segundoNum);
     }
     else if(metodo == 3){
-        printf("o resultado é %d. Pronto, salvei você uma questão da sua tarefa!\n", primeiroNum * segundoNum);
+        printf("o resultado é %d.\n", primeiroNum * segundoNum);
     }
     else if(metodo == 4){
-        printf("o resultado é %.5f e o restante é %d. Pronto, salvei você uma questão da sua tarefa!\n", primeiroNumDiv / segundoNumDiv, primeiroNum % segundoNum);
+        printf("o resultado é %.5f e o restante é %d.\n", primeiroNumDiv / segundoNumDiv, primeiroNum % segundoNum);
     }
 
     return 0;
